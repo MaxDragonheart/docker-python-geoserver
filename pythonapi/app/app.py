bbf7b2006550731f11414aa12dd13d932e8a711d
MaxDragonheart <massimilianomoraca@gmail.com>
2022-12-17 11:37:09 +0100

    feat get json for html page

--- pythonapi/app/app.py
+++ pythonapi/app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
-from conf import DOMAIN, WORKSPACE, SERVICE_VERSION, LAYER_NAME
+from conf import DOMAIN, WORKSPACE, SERVICE_VERSION, LAYER_NAME, MAIN_PATH, PYTHON_API_PORT
 from api import get_wms_data
 
 app = Flask(__name__)
@@ -10,16 +10,21 @@ WMS_URL = f"{DOMAIN}/geoserver/{WORKSPACE}/wms"
 
 @app.route('/')
 def start_api():
-    output = get_wms_data(
-        wms_url=WMS_URL,
-        service_version=SERVICE_VERSION,
-        layer_name=LAYER_NAME,
-    )
-    output['wms-url'] = WMS_URL
-    output['wms-layer'] = LAYER_NAME
+    # output = get_wms_data(
+    #     wms_url=WMS_URL,
+    #     service_version=SERVICE_VERSION,
+    #     layer_name=LAYER_NAME,
+    # )
+    # output['wms-url'] = WMS_URL
+    # output['wms-layer'] = LAYER_NAME
 
-    return output
+    template_path = MAIN_PATH.joinpath('./templates/index.html')
+    if not template_path.exists():
+        raise Exception(f"Template path doesn't exists.\n Path: {template_path}")
+
+    #return output
+    return render_template(str(template_path), title=LAYER_NAME)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=PYTHON_API_PORT, debug=True)
